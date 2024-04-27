31e50f1534e340c890cbf861ee3a0d4bc3effb55
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-27 05:10:09 +0000

    [Finish]5-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_new = Book.new
   end
-   
+
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -28,15 +32,15 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destoy
     redirect_to books_path
   end
 
@@ -45,4 +49,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @bookuser == current_user
+      redirect_to books_path
+    end
+  end
+  
 end
